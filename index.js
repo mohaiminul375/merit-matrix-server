9020b1d02b80af35679040309900a67ca7a35eb7
Mohaiminul375 <mohaiminul375@gmail.com>
2024-06-02 20:55:40 +0600

    data post successfully

--- index.js
+++ index.js
@@ -4,12 +4,14 @@ const app = express();
 const cors = require("cors");
 require("dotenv").config();
 const port = process.env.PORT || 3000;
-
+const corsOptions = {
+  origin: ["http://localhost:5173"],
+  optionSuccessStatus: 200,
+};
 app.use(express.json());
-app.use(cors());
+app.use(cors(corsOptions));
 
-const uri =
-  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ixszr3u.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ixszr3u.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -23,7 +25,21 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    // await client.connect();
+
+    // collection
+
+    const scholarshipCollection = client
+      .db("merit-matrix")
+      .collection("all-scholarship");
+    // scholarship manage
+
+    // data post to server
+    app.post("/all-scholarship", async (req, res) => {
+      const scholarship = req.body;
+      const result = await scholarshipCollection.insertOne(scholarship);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
